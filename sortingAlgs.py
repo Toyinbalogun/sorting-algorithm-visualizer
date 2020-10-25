37b7145cd3a8e0b552f6ae7a89ca459c711c4d58
TyBalogun <Balogunt302@gmail.com>
2020-10-25 06:02:00 -0400

    created a empty gui window

---
+++ sortingAlgs.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+import random 
+
+root = Tk()
+root.title('Sorting Algorithm Visualizer')
+root.maxsize(900, 600)
+root.config(bg='black')
+
+root.mainloop()
