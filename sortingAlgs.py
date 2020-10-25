6b34f087725be8d23e404fbb3e10458bd322349f
TyBalogun <Balogunt302@gmail.com>
2020-10-25 06:21:09 -0400

    split window into grid and canvas

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -7,4 +7,11 @@ root.title('Sorting Algorithm Visualizer')
 root.maxsize(900, 600)
 root.config(bg='black')
 
+#frame and base layout
+UI_frame = Frame(root, width= 600, height=200, bg='grey')
+UI_frame.grid(row=0, column=0,padx=10, pady=5)
+
+canvas = Canvas(root, width=600, height=380, bg='white')
+canvas.grid(row=1,column=0, padx=10, pady=5)
+
 root.mainloop()
