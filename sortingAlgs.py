d61745c18b24f773ece34dfa17e8ec753fa319c2
TyBalogun <Balogunt302@gmail.com>
2020-10-28 14:15:18 -0400

    started mapping UI area

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -14,4 +14,11 @@ UI_frame.grid(row=0, column=0,padx=10, pady=5)
 canvas = Canvas(root, width=600, height=380, bg='white')
 canvas.grid(row=1,column=0, padx=10, pady=5)
 
+#User Interface Area
+#Row[0]
+
+#Row[1]
+
+
+
 root.mainloop()
