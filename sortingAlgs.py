cac16729aff725780112c044f1ad7b0cf3c1af25
Toyinbalogun <balogunt302@gmail.com>
2020-10-30 23:25:07 -0400

    fixed syntax error 'vlues' to 'values'

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -7,6 +7,9 @@ root.title('Sorting Algorithm Visualizer')
 root.maxsize(900, 600)
 root.config(bg='black')
 
+#variables
+selected_alg = StringVar()
+
 #frame and base layout
 UI_frame = Frame(root, width= 600, height=200, bg='grey')
 UI_frame.grid(row=0, column=0,padx=10, pady=5)
@@ -17,8 +20,9 @@ canvas.grid(row=1,column=0, padx=10, pady=5)
 #User Interface Area
 #Row[0]
 Label(UI_frame, text="Algorithm: ", bg='grey').grid(row=0,column=0, padx=10, pady=5, sticky=W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, vlues=['Bubble Sort', 'Merge Sort'])
-algMenu.grid(row=0)
+algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Merge Sort'])
+algMenu.grid(row=0, column=1, padx=5, pady=5)
+algMenu.current(0)
 #Row[1]
 
 
