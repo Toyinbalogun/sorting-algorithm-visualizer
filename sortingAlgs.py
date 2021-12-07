b7b672fd835924fd214aa0873cfa8c4f45e62441
Toyin Balogun <balogunt302@gmail.com>
2021-12-07 13:24:22 -0500

    added bubblesort implementation to tk window

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -1,98 +1,88 @@
 from tkinter import *
 from tkinter import ttk
-import random 
+import random
+from bubbleSort import bubble_sort 
 
-root = Tk()
-root.title('Sorting Algorithm Visualizer')
+root =Tk()
+root.title("Sorting Algorithm Visualizer")
 root.maxsize(900, 600)
-root.config(bg='black')
+root.config(bg="black")
 
 #variables
 selected_alg = StringVar()
+data = []
 
-#functions
-def drawData(data):
+def drawData(data, colorArray):
     canvas.delete("all")
-    c_height = 380
-    c_width = 600
-    x_width = c_width/ (len(data) + 1)
+    cHeight = 380
+    cWidth = 600
+    xWidth = cWidth / (len(data) + 1)
     offset = 30
     spacing = 10
-
-    #normalize data
-    normalizeData = [i/max(data) for i in data]
-
-    for i, height in enumerate(normalizeData):
+    normalizedData = [ i / max(data) for i in data]
+    for i, height in enumerate(normalizedData):
         #top left
-        x0 = i * x_width + offset + spacing
-        y0 = c_height - height * 340
+        x0 = i * xWidth + offset + spacing
+        y0 = cHeight - height * 340
 
         #bottom right
-        x1 = (i + 1) * x_width + offset 
-        y1 = c_height 
+        x1 = (i + 1) * xWidth + offset
+        y1 = cHeight
 
-        canvas.create_rectangle(x0, y0, x1, y1, fill="red")
-        canvas.create_text(x0+2, y0, anchor=SW, text= str(data[i]))
-
+        canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
+        canvas.create_text(x0+2, y0, anchor=SW, text=str(data[i]))
+   
+    root.update_idletasks()
 
 
 def Generate():
-    print("Alg Selected: " + selected_alg.get())
-    try:
-        minVal = int(minEntry.get())
-    except:
-        minVal = 1
-    try:
-        maxVal = int(maxEntry.get())
-    except:
-        maxVal = 10
-    try:
-        size = int(sizeEntry.get())
-    except:
-        size = 10
+    #print("Alg Selected: " + selected_alg.get())
+    global data
 
-    if minVal < 0 : minVal = 0
-    if maxVal > 100 : maxVal = 100
-    if size > 30 or size < 3: size = 25
-    if minVal > maxVal : minVal, maxVal = maxVal, minVal
+    minVal = int(minEntry.get())
+    maxVal = int(maxEntry.get())
+    size = int(sizeEntry.get())
 
-    data =[]
+    data = []
     for _ in range(size):
-        data.append(random.randrange(minVal, maxVal+1)) #maxVal inclusive
+        data.append(random.randrange(minVal, maxVal+1))
     
-    # === TEST ===
-    # data = [1, 2, 4, 6]
-    drawData(data)
+    drawData(data, ["red" for x in range(len(data))])
 
-#frame and base layout
-UI_frame = Frame(root, width= 600, height=200, bg='grey')
-UI_frame.grid(row=0, column=0,padx=10, pady=5)
+def StartAlgorithm():
+    global data
+    bubble_sort(data, drawData, speedScale.get())
 
-canvas = Canvas(root, width=600, height=380, bg='white')
-canvas.grid(row=1,column=0, padx=10, pady=5)
+#UI frame / base layout
+UI_frame = Frame(root, width=600, height=200, bg="grey")
+UI_frame.grid(row=0, column=0, padx=10, pady=5)
+
+canvas = Canvas(root, width=600, height=380, bg="white")
+canvas.grid(row=1, column=0, padx=10, pady=5)
 
 #User Interface Area
-
 #Row[0]
-Label(UI_frame, text="Algorithm : ", bg='grey').grid(row=0,column=0, padx=10, pady=5, sticky=W)
-
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Merge Sort'])
+Label(UI_frame, text="Algorithm: ", bg="grey").grid(row=0, column=0, padx=5, pady=5, sticky=W)
+algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=["Bubble Sort", "Merge Sort"])
 algMenu.grid(row=0, column=1, padx=5, pady=5)
 algMenu.current(0)
 
-Button(UI_frame, text="Generate", command=Generate, bg='red' ).grid(row=0, column=2, padx=5, pady=5)
+speedScale = Scale(UI_frame, from_=0.1, to=2.0, length=200, digits=2, resolution=0.2, orient=HORIZONTAL, label="Select Speed [s]")
+speedScale.grid(row=0, column=2, padx=5, pady=5)
+Button(UI_frame, text="Start", command=StartAlgorithm, bg="red").grid(row=0, column=3, padx=5, pady=5)
 
 #Row[1]
-Label(UI_frame, text="Size : ", bg='grey').grid(row=1,column=0, padx=10, pady=5, sticky=W)
-sizeEntry = Entry(UI_frame)
-sizeEntry.grid(row=1,column=1, padx=10, pady=5, sticky=W)
+sizeEntry = Scale(UI_frame, from_=3, to=25, resolution=1, orient=HORIZONTAL, label="Data Size")
+sizeEntry.grid(row=1, column=0, padx=5, pady=5)
 
-Label(UI_frame, text="Min Value : ", bg='grey').grid(row=1,column=2, padx=10, pady=5, sticky=W)
-minEntry = Entry(UI_frame)
-minEntry.grid(row=1,column=3, padx=10, pady=5, sticky=W)
 
-Label(UI_frame, text="Max Value : ", bg='grey').grid(row=1,column=4, padx=10, pady=5, sticky=W)
-maxEntry = Entry(UI_frame)
-maxEntry.grid(row=1,column=5, padx=10, pady=5, sticky=W)
+minEntry = Scale(UI_frame, from_=0, to=10, resolution=1, orient=HORIZONTAL, label="Min Value")
+minEntry.grid(row=1, column=1, padx=5, pady=5)
+
+
+maxEntry = Scale(UI_frame, from_=10, to=100, resolution=1, orient=HORIZONTAL, label="Max Value")
+maxEntry.grid(row=1, column=2, padx=5, pady=5)
+
+Button(UI_frame, text="Generate", command=Generate, bg="white").grid(row=1, column=3, padx=5, pady=5)
 
 root.mainloop()
